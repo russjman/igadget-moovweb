1486b18aefe0337c154ba833a8f63c2a9a37e578
Russell Mantilla <russjman@gmail.com>
2017-01-25 13:21:49 -0800

    Removed broken carousel on product page

--- scripts/pages/product.ts
+++ scripts/pages/product.ts
@@ -4,28 +4,4 @@ $("./body") {
 		remove()
 	}
 
-	#convert all right column panels to ur carousels
-	$(".//div[contains(@class,'Right')]"){
-		$("./div[contains(@style,'display:none;')]") {
-				log('found hidden div')
-				log(fetch('@id'))
-
-				remove()
-		}
-		$("./div[contains(@class,'Panel')]") {
-
-			attribute("data-ur-set","carousel")
-			attribute("data-ur-fill","4")
-
-			$(".//ul"){
-				attribute("data-ur-carousel-component","scroll_container")
-				$("./li"){
-					attribute("data-ur-carousel-component","item")
-				}
-			}
-			insert("a",class:"prev ",data-ur-carousel-component:"button",data-ur-carousel-button-type:"prev")
-			insert("a",class:"next",data-ur-carousel-component:"button",data-ur-carousel-button-type:"next")
-		}
-		
-	}
 }
